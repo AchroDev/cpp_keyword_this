39975665a3178f61175894fb342b3141c76aef9b
AchroDev <achrodev@proton.me>
2024-10-06 16:22:08 -0500

    Added example code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <string>
+
+// The "this" keyword is a pointer to the current object instance that the method belongs to.
+
+class Entity
+{
+public:
+    int x, y;
+
+    Entity(int x, int y)
+    {
+        x = x; // Currently we are just assigning the x in the parameters here
+    }
+};
 
 int main()
 {
