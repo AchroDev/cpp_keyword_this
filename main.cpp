f9ec0472bf5a2db4feb977676b0f16e43eec1c2b
AchroDev <achrodev@proton.me>
2024-10-06 16:38:53 -0500

    Removed code in Getter method

--- main.cpp
+++ main.cpp
@@ -21,10 +21,6 @@ public:
     // Example function to return one of the variables
     int GetX() const
     {
-        const Entity *e = this; // In a const function like this one, "this" actually has to be a const as well
-        e->x = 3;               // Now that Entity e is a const, the compiler WILL NOT allow us to modify the variable, as we shouldn't anyway.
-
-        return x;
     }
 };
 
