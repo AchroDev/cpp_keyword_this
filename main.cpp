6a0a66aeaa13cc521ad184c7e3f0828c5aeb5030
AchroDev <achrodev@proton.me>
2024-10-06 16:34:40 -0500

    Assigned y in the Entity constructor and added example Getter method

--- main.cpp
+++ main.cpp
@@ -10,8 +10,18 @@ public:
 
     Entity(int x, int y)
     {
-        this->x = x;   // To clean it up we can really write it like this as the previous code showed how "this" worked
-        (*this).x = x; // Alternatively you can cast "this" to a pointer and call assign x this way, but it isn't as clean as dereferencing with the arrow
+        this->x = x; // To clean it up we can really write it like this as the previous code showed how "this" worked
+        //(*this).x = x; // Alternatively you can cast "this" to a pointer and call assign x this way, but it isn't as clean as dereferencing with the arrow
+        this->y = y;
+    }
+
+    // Example function to return one of the variables
+    int GetX() const
+    {
+        Entity *e = this; // In a const function like this one, "this" actually has to be a const as well
+        e->x = 3;         // With Entity e not being a const, the compiler would allow you to modify a variable you shouldn't be modifying
+
+        return x;
     }
 };
 
